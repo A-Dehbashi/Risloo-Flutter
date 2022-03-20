fcceafe89f72082b9d27db7fbbb3a7f967e42907
Amir Dehbashi <a.dehbashi100@gmail.com>
2022-03-20 14:35:25 +0330

    fixed some stuffs
    part 2

--- lib/views/components/component_home_toolbar.dart
+++ lib/views/components/component_home_toolbar.dart
@@ -6,10 +6,9 @@ import 'package:risloo_flutter/utils/res/res_lists.dart';
 
 class ComponentHomeToolbar extends StatelessWidget {
 
-  final List<int> values = [0, 1, 2, 3];
-
   // Vars
   final Function callback;
+  final List<int> values = [0, 1, 2, 3];
 
   // Constructor
   ComponentHomeToolbar({Key? key, required this.callback}) : super(key: key);
@@ -17,88 +16,99 @@ class ComponentHomeToolbar extends StatelessWidget {
   // Tree
   @override
   Widget build(BuildContext context) {
-    return DropdownMenuItem(
-      child: Container(
-        height: 48.0,
-        margin: const EdgeInsets.fromLTRB(12.0, 12.0, 6.0, 0.0),
+    return Container(
+      height: 48.0,
+      margin: const EdgeInsets.fromLTRB(12.0, 12.0, 6.0, 0.0),
 
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(4.0),
-            border: Border.all(color: ResColors.coolGray300, width: 1.0),
-            shape: BoxShape.rectangle
-        ),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(4.0),
+          border: Border.all(
+            color: ResColors.coolGray300,
+            width: 1.0,
+          ),
+          shape: BoxShape.rectangle,
+      ),
 
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(8.0, 6.0, 8.0, 6.0),
-          child: Stack(
-            children: <Widget>[
-              SingleChildScrollView(
-                child: Row(
-                  children: <Widget>[
-                    Column(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const <Widget>[
-                        Text(
-                          "امیر دهباشی",
-                          textDirection: TextDirection.rtl,
-                          style: TextStyle(
-                            color: ResColors.coolGray700,
-                            fontSize: 11.0,
-                            fontFamily: ResFonts.danaDemiBold,
+      child: Material(
+        color: ResColors.transparent,
+        child: InkWell(
+          highlightColor: ResColors.transparent,
+          splashColor: ResColors.coolGray300,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(8.0, 6.0, 8.0, 6.0),
+            child: Stack(
+              children: <Widget>[
+                SingleChildScrollView(
+                  child: Row(
+                    children: <Widget>[
+                      Column(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const <Widget>[
+                          Text(
+                            "امیر دهباشی",
+                            textDirection: TextDirection.rtl,
+                            style: TextStyle(
+                              color: ResColors.coolGray700,
+                              fontSize: 11.0,
+                              fontFamily: ResFonts.danaDemiBold,
+                            ),
                           ),
-                        ),
-                        Text(
-                          "100.000 تومان",
-                          textDirection: TextDirection.rtl,
-                          style: TextStyle(
-                            color: ResColors.emerald600,
-                            fontSize: 11.0,
-                            fontFamily: ResFonts.danaDemiBold,
-                          ),
-                        )
-                      ],
-                    ),
-                    const SizedBox(width: 10.0),
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(4.0),
-                      child: Image.asset(
-                        "assets/icon.png",
-                        width: 32.0,
-                        height: 32.0,
+                          Text(
+                            "100.000 تومان",
+                            textDirection: TextDirection.rtl,
+                            style: TextStyle(
+                              color: ResColors.emerald600,
+                              fontSize: 11.0,
+                              fontFamily: ResFonts.danaDemiBold,
+                            ),
+                          )
+                        ],
                       ),
-                    )
-                  ],
-                ),
-              ),
-              DropdownButtonHideUnderline(
-                child: ButtonTheme(
-                  alignedDropdown: true,
-                  child: DropdownButton<int>(
-                    iconSize: 0,
-                    items: values.map((index) => DropdownMenuItem<int>(
-                      value: index,
-                      child: Align(
-                        alignment: Alignment.center,
-                        child: Text(
-                          ResLists.toolbarTitles[index],
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: index == 3 ? ResColors.red600 : ResColors.coolGray700,
-                            fontSize: 13.0,
-                            fontFamily: ResFonts.danaMedium,
-                          ),
+                      const SizedBox(width: 10.0),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(4.0),
+                        child: Image.asset(
+                          "assets/icon.png",
+                          width: 32.0,
+                          height: 32.0,
                         ),
-                      ),
-                    )).toList(),
-                    onChanged: (index) {
-                      callback(index);
-                    },
+                      )
+                    ],
                   ),
                 ),
-              ),
-            ],
+                DropdownButtonHideUnderline(
+                  child: ButtonTheme(
+                    alignedDropdown: true,
+                    child: DropdownButton<int>(
+                      iconSize: 0,
+                      items: values.map((index) => DropdownMenuItem<int>(
+                        value: index,
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            ResLists.toolbarTitles[index],
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: index == 3 ? ResColors.red600 : ResColors.coolGray700,
+                              fontSize: 13.0,
+                              fontFamily: ResFonts.danaMedium,
+                            ),
+                          ),
+                        ),
+                      )).toList(),
+                      onChanged: (index) {
+                        callback(index);
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
+          onTap: () {
+            // TODO: Place Code When Needed
+          },
         ),
       ),
     );
